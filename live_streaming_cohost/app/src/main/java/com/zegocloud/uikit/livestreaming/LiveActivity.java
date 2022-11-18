0101f362e53f4f0e55f42361ca1b1ae382e96a78
alan <alanliu@zego.im>
2022-11-18 18:36:00 +0800

    update cohost example

---
+++ live_streaming_cohost/app/src/main/java/com/zegocloud/uikit/livestreaming/LiveActivity.java
@@ -0,0 +1,50 @@
+package com.zegocloud.uikit.livestreaming;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.zegocloud.uikit.plugin.common.IZegoUIKitPlugin;
+import com.zegocloud.uikit.plugin.signaling.ZegoSignalingPlugin;
+import com.zegocloud.uikit.prebuilt.livestreaming.ZegoUIKitPrebuiltLiveStreamingConfig;
+import com.zegocloud.uikit.prebuilt.livestreaming.ZegoUIKitPrebuiltLiveStreamingFragment;
+import java.util.Collections;
+import java.util.List;
+
+public class LiveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_live);
+
+        addFragment();
+    }
+
+    private void addFragment() {
+        long appID = getIntent().getLongExtra("appID", 0L);
+        String appSign = getIntent().getStringExtra("appSign");
+        String userID = getIntent().getStringExtra("userID");
+        String userName = getIntent().getStringExtra("userName");
+
+        boolean isHost = getIntent().getBooleanExtra("host", false);
+        String liveID = getIntent().getStringExtra("liveID");
+        //        long appID = YourAppID;
+        //        String appSign = YourAppSign;
+        //
+        //        String liveID = yourLiveID;
+        //        String userID = YourUserID;
+        //        String userName = YourUserName;
+
+        ZegoUIKitPrebuiltLiveStreamingConfig config;
+        List<IZegoUIKitPlugin> plugins = Collections.singletonList(ZegoSignalingPlugin.getInstance());
+        if (isHost) {
+            config = ZegoUIKitPrebuiltLiveStreamingConfig.host(plugins);
+        } else {
+            config = ZegoUIKitPrebuiltLiveStreamingConfig.audience();
+            ;
+        }
+
+        ZegoUIKitPrebuiltLiveStreamingFragment fragment = ZegoUIKitPrebuiltLiveStreamingFragment.newInstance(appID,
+            appSign, userID, userName, liveID, config);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commitNow();
+    }
+}
---
+++ live_streaming_cohost/app/src/main/java/com/zegocloud/uikit/livestreaming/MainActivity.java
@@ -0,0 +1,57 @@
+package com.zegocloud.uikit.livestreaming;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        long appID = ;
+        String appSign = ;
+
+        String userID = Build.MANUFACTURER + "_" + generateUserID();
+        String userName = userID + "_Name";
+        String liveID = "test_live_id";
+
+
+        findViewById(R.id.start_live).setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, LiveActivity.class);
+            intent.putExtra("host", true);
+            intent.putExtra("appID", appID);
+            intent.putExtra("appSign", appSign);
+            intent.putExtra("userID", userID);
+            intent.putExtra("userName", userName);
+            intent.putExtra("liveID", liveID);
+            startActivity(intent);
+        });
+        findViewById(R.id.watch_live).setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, LiveActivity.class);
+            intent.putExtra("appID", appID);
+            intent.putExtra("appSign", appSign);
+            intent.putExtra("userID", userID);
+            intent.putExtra("userName", userName);
+            intent.putExtra("liveID", liveID);
+            startActivity(intent);
+        });
+    }
+
+    private String generateUserID() {
+        StringBuilder builder = new StringBuilder();
+        Random random = new Random();
+        while (builder.length() < 5) {
+            int nextInt = random.nextInt(10);
+            if (builder.length() == 0 && nextInt == 0) {
+                continue;
+            }
+            builder.append(nextInt);
+        }
+        return builder.toString();
+    }
+}
